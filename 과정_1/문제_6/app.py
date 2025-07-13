df410b54b3ddcc40f0a32041144c512cb4478544
최종환 <whdghks1999@konkuk.ac.kr>
2025-07-13 17:14:36 +0900

    Edit port

---
+++ 과정_1/문제_6/app.py
@@ -0,0 +1,20 @@
+# app.py
+from flask import Flask, request, Response
+import os
+from io import BytesIO
+from gtts import gTTS
+
+DEFAULT_LANG = os.getenv('DEFAULT_LANG', 'ko')
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    text = "Hello, DevOps"
+    lang = request.args.get('lang', DEFAULT_LANG)
+    fp = BytesIO()
+    gTTS(text, lang=lang, tld="com").write_to_fp(fp)
+    fp.seek(0)
+    return Response(fp.getvalue(), mimetype='audio/mpeg')
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)  # ← 포트 변경됨
